45ed1b0b7c1d39fdff70d3b6c322efb7596d0278
shubhamgoel7703 <shubhamgoel7703@gmail.com>
2020-08-11 19:51:03 +0530

    project name modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spaceXLaunchPrograms'`, () => {
+  it(`should have as title 'spaceXLaunch'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('spaceXLaunchPrograms');
+    expect(app.title).toEqual('spaceXLaunch');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spaceXLaunchPrograms!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to spaceXLaunch!');
   });
 });
